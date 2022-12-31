48a1eb7230fd8914d12e7629f0bff3e7c4f8a630
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-12-30 21:25:54 -0600

    Game over screen and font update

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
@@ -25,7 +26,7 @@ public class GameTimer : MonoBehaviour
         }
         else
         {
-            gameTimer.fillAmount = 0;
+            SceneManager.LoadScene(5);
         }
     }
 }
