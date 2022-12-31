755853a97a1dee92446a378c496bf2c5e657fe6c
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-12-31 09:11:32 -0600

    Title screen alignment

--- Assets/Scripts/UpdateMoney.cs
+++ Assets/Scripts/UpdateMoney.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UpdateMoney : MonoBehaviour
 {
     public static int customersServed;
     public static int totalEarned;
     public static int tipsEarned;
-    public Text UItext;
+    public TMP_Text UItext;
 
     private void Start()
     {
